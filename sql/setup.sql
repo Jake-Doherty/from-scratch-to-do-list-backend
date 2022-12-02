5c04e9adef1b3f533862c82e696d7ce62960c18c
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-12-02 15:29:38 -0800

    build todos table and set remove .example

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+
+DROP TABLE IF EXISTS todos CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,4 +10,12 @@ CREATE TABLE users (
   password_hash VARCHAR NOT NULL,
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL
+);
+
+CREATE TABLE todos (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT,
+  detail VARCHAR,
+  completed BOOLEAN,
+  FOREIGN KEY (user_id) REFERENCES users(id) 
 );
